47c337a3ae395378920d77cab91ff605e8746bd2
unknown <dimashama@gmail.com>
2021-10-24 17:36:44 +0200

    '3-select'

--- src/App.js
+++ src/App.js
@@ -2,26 +2,50 @@ import React, {useState} from 'react'
 import Counter from "./components/Counter";
 import './styles/App.css'
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 const App = () => {
 
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'}
+        {id: 1, title: 'аааа', body: 'яяяя'},
+        {id: 2, title: 'бббб 2', body: 'дддд'},
+        {id: 3, title: 'яяяя 3', body: 'ееее'}
     ])
+    const [selectedSort, setSelectedSort] = useState('')
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
 
     return (
         <div className="App">
             {/*<Counter />*/}
-            <form>
-                <MyInput type="text" placeholder="название"/>
-                <MyInput type="text" placeholder="описание"/>
-                <MyButton disabled>Создать пост</MyButton>
-            </form>
-            <PostList posts={posts} title={'Список постов 1'}/>
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MySelect defaultValue='Сортировка'
+                          value={selectedSort}
+                          onChange={sortPosts}
+                          option={[
+                              {value: 'title', name: 'По названию'},
+                              {value: 'body', name: 'По описанию'}
+                          ]}/>
+            </div>
+            {posts.length !== 0
+                ? <PostList remove={removePost} posts={posts} title={'Список постов 1'}/>
+                : <h1 style={{textAlign: 'center'}}>Список постов пуст!</h1>
+            }
+
         </div>
     )
 }
